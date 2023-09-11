c6f99bb9ea5880a249ec1df9ab376c1002b2b546
Garrett Girod <girodg@hotmail.com>
2023-09-11 10:26:29 -0400

    geters/setters, methods

--- Week 3/Day07/Day07/Person.cpp
+++ Week 3/Day07/Day07/Person.cpp
@@ -1,1 +1,8 @@
 #include "Person.h"
+#include <iostream>
+
+void Person::ItsMyBirthday()
+{
+	++age_;
+	std::cout << "It's my birthday!! I'm turning " << age_ << " today!! Let's have some cake!\n";
+}
--- Week 3/Day07/Day07/Person.h
+++ Week 3/Day07/Day07/Person.h
@@ -3,6 +3,21 @@
 
 class Person
 {
+public:
+	int GetAge() const { return age_; }
+	void SetAge(int age)
+	{
+		if (age >= 0 && age <= 120)
+			age_ = age;
+	}
+	std::string GetName() const { return _Name; }
+	void SetName(std::string newName)
+	{
+		_Name = newName;
+	}
+
+	void ItsMyBirthday();
+
 private:
 	int age_;
 	std::string _Name;
